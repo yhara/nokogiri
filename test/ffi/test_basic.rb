7099a9bddd391d00e2be4b02f94fc5bd0e1610e0
Mike Dalessio <mike@csa.net>
2008-12-02 20:18:09 -0500

    test cleanup

--- test/ffi/test_basic.rb
+++ test/ffi/test_basic.rb
@@ -4,25 +4,29 @@ require File.expand_path(File.join(File.dirname(__FILE__), "..", "helper"))
 
 class TestFFI
   class TestBasic < Nokogiri::TestCase
-    LOOP_COUNT = 100
-    WIGGLE_ROOM = 2
+
+    def setup
+      @loop_count = 100
+      @wiggle_room = 2
+    end
     
-    def test_basic_html
-      Nokogiri::LibXML::XmlDoc.expects(:release).at_least(LOOP_COUNT - WIGGLE_ROOM)
-      LOOP_COUNT.times do
+    def test_serialize_and_GC_HTML_documents
+      Nokogiri::LibXML::HtmlDocument.expects(:release).at_least(@loop_count - @wiggle_room)
+      @loop_count.times do
         doc = Nokogiri::HTML::Document.read_memory(File.read(HTML_FILE), nil, nil, 2145)
         assert_equal 66734, doc.serialize.size
       end
       10.times { GC.start }
     end
 
-    def test_basic_xml
-      Nokogiri::LibXML::XmlDoc.expects(:release).at_least(LOOP_COUNT - WIGGLE_ROOM)
-      LOOP_COUNT.times do
+    def test_serialize_and_GC_XML_documents
+      Nokogiri::LibXML::XmlDocument.expects(:release).at_least(@loop_count - @wiggle_room)
+      @loop_count.times do
         doc = Nokogiri::XML::Document.read_memory(File.read(XML_FILE), nil, nil, 2145)
         assert_equal 1953, doc.serialize.size
       end
       10.times { GC.start }
     end
+
   end
 end
