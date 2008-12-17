db651a9940a656f7a21c9d642573f7076abc8cd8
Mike Dalessio <mike@csa.net>
2008-12-16 23:28:15 -0500

    removing TODO comments

--- lib/nokogiri/ffi/html/sax/parser.rb
+++ lib/nokogiri/ffi/html/sax/parser.rb
@@ -4,14 +4,12 @@ module Nokogiri
       class Parser
         
         def native_parse_file data, encoding
-          # TODO: isn't it more interesting to return the doc tree than the data we passed in?
           docptr = LibXML.htmlSAXParseFile(data, encoding, cstruct, nil)
           LibXML.xmlFreeDoc docptr
           data
         end
 
         def native_parse_memory data, encoding
-          # TODO: isn't it more interesting to return the doc tree than the data we passed in?
           docptr = LibXML.htmlSAXParseDoc(data, encoding, cstruct, nil)
           LibXML.xmlFreeDoc docptr
           data
