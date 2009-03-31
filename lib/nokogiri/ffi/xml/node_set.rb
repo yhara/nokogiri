a8b2486bfc8c8ebec2c51f4e8c85942d9e64d60c
Mike Dalessio <mike@csa.net>
2009-03-31 18:35:16 -0400

    FFI: finished code review of xml/node_set.rb

--- lib/nokogiri/ffi/xml/node_set.rb
+++ lib/nokogiri/ffi/xml/node_set.rb
@@ -4,33 +4,41 @@ module Nokogiri
 
       attr_accessor :cstruct
 
-      def self.new document, list = []
-        set = allocate
-        set.document = document
-        set.cstruct = LibXML::XmlNodeSet.new(LibXML.xmlXPathNodeSetCreate(nil))
-        list.each { |x| set << x }
-        yield set if block_given?
+      def dup
+        dup = LibXML.xmlXPathNodeSetCreate(nil)
+        cstruct.nodeTab.each do |node|
+          LibXML.xmlXPathNodeSetAdd(dup, node)
+        end
+        
+        set = NodeSet.allocate
+        set.cstruct = LibXML::XmlNodeSet.new(dup)
         set
       end
 
       def length
-        return 0 if cstruct.pointer.null?
-        return cstruct[:nodeNr]
+        cstruct.pointer.null? ? 0 : cstruct[:nodeNr]
       end
 
-      def [] number
-        return nil if cstruct.pointer.null?
+      def push(node)
+        raise ArgumentError("node must be a Nokogiri::XML::Node") unless node.is_a?(XML::Node)
+        LibXML.xmlXPathNodeSetAdd(cstruct, node.cstruct)
+        self
+      end
+
+      def [](number)
         return nil if (number >= cstruct[:nodeNr] || number.abs > cstruct[:nodeNr])
         number = number + cstruct[:nodeNr] if number < 0
         Node.wrap(cstruct.nodeTab[number])
       end
 
-      def push(node)
-        LibXML.xmlXPathNodeSetAdd(cstruct, node.cstruct)
-        self
+      def to_a
+        cstruct.nodeTab.collect { |node| Node.wrap(node) }
       end
 
       def unlink
+        # TODO: is this simpler implementation viable:
+        #  cstruct.nodeTab.collect {|node| Node.wrap(node)}.each(&:unlink)
+        # ?
         nodetab = cstruct.nodeTab
         cstruct[:nodeNr].times do |j|
           node = Node.wrap(nodetab[j])
@@ -39,6 +47,16 @@ module Nokogiri
         end
         cstruct.nodeTab = nodetab
         self
+      end
+
+
+      def self.new document, list = []
+        set = allocate
+        set.document = document
+        set.cstruct = LibXML::XmlNodeSet.new(LibXML.xmlXPathNodeSetCreate(nil))
+        list.each { |x| set << x }
+        yield set if block_given?
+        set
       end
 
     end
